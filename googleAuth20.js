7a51e01364b659dac6da88063b94c5aa26a0944c
ROHIT SHARMA <102727841+rohitsharma716@users.noreply.github.com>
2025-02-15 16:46:28 +0530

    googleAuth2.0

---
+++ googleAuth20.js
@@ -0,0 +1,67 @@
+require("dotenv").config();
+const express = require("express")
+const passport =  require("passport")
+const session = require("express-session")
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+
+const app = express();
+
+app.use(
+    session({
+        secret:"!@#@$#$#@",
+        resave: false,
+        saveUninitialized:true
+    })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+    new GoogleStrategy(
+        {
+            
+            clientID:process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "http://localhost:3000/auth/google/callback"
+        },
+        (accessToken, refreshToken, profile, done) =>{
+            return done(null,profile);
+        }
+    )
+);
+
+passport.serializeUser((user,done) => done(null,user));
+passport.deserializeUser((user,done) => done(null,user));
+
+app.get("/",(req,res) =>{
+     res.send("<a href='/auth/google'> LOGIN WITH GOOGLE  </a>")
+});
+
+app.get(
+    "/auth/google",
+    passport.authenticate("google",{scope: ["profile","email"]})
+)
+
+app.get(
+    "/auth/google/callback",
+    passport.authenticate("google",{failureRedirect:"/"}),
+    (req,res)=>{
+        res.redirect("/profile");
+    }
+)
+
+app.get("/profile", (req,res)=> {
+     res.send(`Welcome ${req.user.displayName}`)
+})
+
+app.get("/logout", (req,res)=>{
+     req.logout( () => {
+         res.redirect("/");
+     })
+})
+
+app.listen(3000 , (req,res) =>{
+    console.log("server is running on port no :  3000")
+})
